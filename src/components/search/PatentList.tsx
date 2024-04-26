10ed94f530b80afe47b0bef5b47cff07e01ffcf7
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-04-26 17:38:23 +1000

    abstract, applicants, and inventors in dropdown

--- src/components/search/PatentList.tsx
+++ src/components/search/PatentList.tsx
@@ -25,19 +25,53 @@ export function PatentListItem({ hit, zebra }: { hit: PatentHit; zebra?: boolean
   console.log(hit)
   const className = zebra ? '' : 'bg-slate-100 dark:bg-[#26323b]'
   return (
-    <div key={hit.document.record_lens_id} className={cn('p-3 flex flex-col gap-1', className)}>
+    <div key={hit.document.record_lens_id} className={cn('p-3 flex flex-col gap-2', className)}>
       <Link href={`/patent/${hit.document.record_lens_id}`}>
         <h4 className="text-lg">{getTitle(hit)}</h4>
       </Link>
-      <div className="py-2">
+      <div className="flex flex-wrap">
         <PatentPills doc={hit.document} />
       </div>
-      <div className="text-sm">{hit.document.publication_type}</div>
-      <div className="text-sm">
-        Published: {hit.document.date_published || hit.document.year_published || 'unknown'}
+      <div className="flex flex-col">
+        <div className="text-sm">{hit.document.publication_type}</div>
+        <div className="text-sm">
+          Published: {hit.document.date_published || hit.document.year_published || 'unknown'}
+        </div>
+        <div className="text-sm">Inventors: {hit.document.inventor?.map((inventor) => inventor.name).join(', ')}</div>
+        <div className="text-sm">
+          Applicants: {hit.document.applicant?.map((applicant) => applicant.name).join(', ')}
+        </div>
       </div>
-      <div className="text-sm">Inventors: {hit.document.inventor?.map((inventor) => inventor.name).join(', ')}</div>
-      <div className="text-sm">Applicants: {hit.document.applicant?.map((applicant) => applicant.name).join(', ')}</div>
+      <div className={cn('flex flex-col overflow-hidden gap-2', { 'h-0': !isOpen })}>
+        <div className="flex flex-col">
+          <h5 className="font-semibold">Abstract:</h5>
+          <div className="text-sm">{hit.document.abstract.en.map((en) => en.text)}</div>
+        </div>
+        <div className="flex flex-col">
+          <h5 className="font-semibold">Applicants:</h5>
+          <ul>
+            {hit.document.applicant.map((applicant) => {
+              return (
+                <li key={applicant.name} className="text-sm">
+                  {applicant.name}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="flex flex-col">
+          <h5 className="font-semibold">Inventors:</h5>
+          <ul>
+            {hit.document.inventor.map((inventor) => {
+              return (
+                <li key={inventor.name} className="text-sm">
+                  {inventor.name}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
       <button onClick={() => setIsOpen(!isOpen)} className="w-full p-2 flex justify-center items-center">
         <ChevronDown className={cn('transition-transform', { 'rotate-180': isOpen })} />
       </button>
