6cd965c84128effaed6634ea8f1dd3d3dfa99a86
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-04-26 19:53:55 +1000

    separated structure

--- src/components/search/PatentList.tsx
+++ src/components/search/PatentList.tsx
@@ -22,13 +22,29 @@ export function PatentList({ response }: { response: PatentSearchResponse }) {
 
 export function PatentListItem({ hit, zebra }: { hit: PatentHit; zebra?: boolean }) {
   const [isExpanded, setIsExpanded] = useState(false)
-  const [isShowingClaims, setIsShowingClaims] = useState(false)
-
   return (
     <div
       key={hit.document.record_lens_id}
       className={cn('p-4 flex flex-col gap-4', { 'bg-slate-100 dark:bg-[#26323b]': zebra })}
     >
+      <MetaInfo hit={hit} />
+      <div className={cn('flex overflow-hidden items-stretch gap-6 border-t py-4', { hidden: !isExpanded })}>
+        <PatentBody hit={hit} />
+        <ArticleSection hit={hit} />
+      </div>
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        className="w-full p-2 flex justify-center items-center hover:bg-accent"
+      >
+        <ChevronDown className={cn('transition-transform', { 'rotate-180': isExpanded })} />
+      </button>
+    </div>
+  )
+}
+
+function MetaInfo({ hit }: { hit: PatentHit }) {
+  return (
+    <div className="flex flex-col gap-4">
       <Link href={`/patent/${hit.document.record_lens_id}`}>
         <h4 className="text-lg">{getTitle(hit)}</h4>
       </Link>
@@ -57,149 +73,152 @@ export function PatentListItem({ hit, zebra }: { hit: PatentHit; zebra?: boolean
         </div>
         <div className="text-sm">Inventors: {hit.document.inventor?.map((inventor) => inventor.name).join(', ')}</div>
       </div>
-      <div className={cn('flex overflow-hidden items-stretch gap-6 border-t py-4', { hidden: !isExpanded })}>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <h5 className="font-semibold">Abstract:</h5>
-            <div className="text-sm">{hit.document.abstract.en.map((en) => en.text)}</div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h5 className="font-semibold">Claims:</h5>
-            {hit.document.claim.en !== undefined ? (
-              <>
-                <div>
-                  <button
-                    onClick={() => setIsShowingClaims(!isShowingClaims)}
-                    className="bg-primary text-primary-foreground text-sm rounded py-1 px-2 w-[93px]"
-                  >
-                    {isShowingClaims ? 'Hide' : 'Show'} Claims
-                  </button>
-                </div>
-                <div className={cn('line-clamp-6 flex flex-col', { hidden: !isShowingClaims })}>
-                  <ul className="list-decimal pl-6">
-                    {hit.document.claim.en?.map((en, i) => {
-                      return (
-                        <li key={i} className="text-sm">
-                          {en.text}
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </div>
-              </>
-            ) : (
-              <div className="text-sm">Information currently unavailable.</div>
-            )}
-          </div>
-          {hit.document.owner.length > 0 && (
-            <div className="flex flex-col">
-              <h5 className="font-semibold">Owners:</h5>
-              <ul className="list-disc pl-6">
-                {hit.document.owner.map((owner) => {
+    </div>
+  )
+}
+
+function PatentBody({ hit }: { hit: PatentHit }) {
+  const [isShowingClaims, setIsShowingClaims] = useState(false)
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <h5 className="font-semibold">Abstract:</h5>
+        <div className="text-sm">{hit.document.abstract.en.map((en) => en.text)}</div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h5 className="font-semibold">Claims:</h5>
+        {hit.document.claim.en !== undefined ? (
+          <>
+            <div>
+              <button
+                onClick={() => setIsShowingClaims(!isShowingClaims)}
+                className="bg-primary text-primary-foreground text-sm rounded py-1 px-2 w-[93px]"
+              >
+                {isShowingClaims ? 'Hide' : 'Show'} Claims
+              </button>
+            </div>
+            <div className={cn('line-clamp-6 flex flex-col', { hidden: !isShowingClaims })}>
+              <ul className="list-decimal pl-6">
+                {hit.document.claim.en?.map((en, i) => {
                   return (
-                    <li key={owner.name} className="text-sm">
-                      {owner.name}
+                    <li key={i} className="text-sm">
+                      {en.text}
                     </li>
                   )
                 })}
               </ul>
             </div>
-          )}
-          <div className="flex flex-col">
-            <h5 className="font-semibold">Applicants:</h5>
-            <ul className="list-disc pl-6">
-              {hit.document.applicant.map((applicant) => {
-                return (
-                  <li key={applicant.name} className="text-sm">
-                    {applicant.name}
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="flex flex-col">
-            <h5 className="font-semibold">Inventors:</h5>
-            <ul className="list-disc pl-6">
-              {hit.document.inventor.map((inventor) => {
-                return (
-                  <li key={inventor.name} className="text-sm">
-                    {inventor.name}
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          {hit.document['class_cpc.inv_symbol'].length > 0 && (
-            <div className="flex flex-col gap-2">
-              <h5 className="font-semibold">CPC Classifications:</h5>
-              <ul className="flex flex-wrap gap-2">
-                {hit.document.class_cpc.map((cpc) => (
-                  <li
-                    key={cpc.symbol + cpc.action_date}
-                    className="text-sm bg-accent text-accent-foreground border-accent-foreground border px-2 py-0.5 rounded-md"
-                  >
-                    {cpc.symbol}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-          {hit.document['class_ipcr.inv_symbol'].length > 0 && (
-            <div className="flex flex-col gap-2">
-              <h5 className="font-semibold">IPC Classifications:</h5>
-              <ul className="flex flex-wrap gap-2">
-                {hit.document.class_ipcr.map((ipcr) => (
-                  <li
-                    key={ipcr.symbol + ipcr.action_date}
-                    className="text-sm bg-accent text-accent-foreground border-accent-foreground border px-2 py-0.5 rounded-md"
-                  >
-                    {ipcr.symbol}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+          </>
+        ) : (
+          <div className="text-sm">Information currently unavailable.</div>
+        )}
+      </div>
+      {hit.document.owner.length > 0 && (
+        <div className="flex flex-col">
+          <h5 className="font-semibold">Owners:</h5>
+          <ul className="list-disc pl-6">
+            {hit.document.owner.map((owner) => {
+              return (
+                <li key={owner.name} className="text-sm">
+                  {owner.name}
+                </li>
+              )
+            })}
+          </ul>
         </div>
-        <div className="shrink-0 w-[250px] flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <h5 className="font-semibold">Document Preview</h5>
-            <div className="aspect-[1/1.414] border-border border bg-white w-full rounded flex items-start justify-center p-3 text-sm">
-              Preview unavailable
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <h5 className="font-semibold">History</h5>
-            <div className="flex flex-col divide-y">
-              <div className="py-2">
-                <div className="text-sm">Publication: {hit.document.date_published}</div>
-                <div className="text-sm">
-                  {hit.document.jurisdiction} {hit.document.doc_number}
-                </div>
-              </div>
-              <div className="py-2">
-                <div className="text-sm">Application: {hit.document.application_reference.date.toString()}</div>
-                <div className="text-sm">
-                  {hit.document.jurisdiction} {hit.document.doc_number}
-                </div>
-              </div>
-              {hit.document.priority_claim.map((priority) => (
-                <div className="py-2">
-                  <div className="text-sm">Priority: {priority.date.toString()}</div>
-                  <div className="text-sm">
-                    {priority.jurisdiction} {priority.doc_number}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+      )}
+      <div className="flex flex-col">
+        <h5 className="font-semibold">Applicants:</h5>
+        <ul className="list-disc pl-6">
+          {hit.document.applicant.map((applicant) => {
+            return (
+              <li key={applicant.name} className="text-sm">
+                {applicant.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="flex flex-col">
+        <h5 className="font-semibold">Inventors:</h5>
+        <ul className="list-disc pl-6">
+          {hit.document.inventor.map((inventor) => {
+            return (
+              <li key={inventor.name} className="text-sm">
+                {inventor.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {hit.document['class_cpc.inv_symbol'].length > 0 && (
+        <div className="flex flex-col gap-2">
+          <h5 className="font-semibold">CPC Classifications:</h5>
+          <ul className="flex flex-wrap gap-2">
+            {hit.document.class_cpc.map((cpc) => (
+              <li
+                key={cpc.symbol + cpc.action_date}
+                className="text-sm bg-accent text-accent-foreground border-accent-foreground border px-2 py-0.5 rounded-md"
+              >
+                {cpc.symbol}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {hit.document['class_ipcr.inv_symbol'].length > 0 && (
+        <div className="flex flex-col gap-2">
+          <h5 className="font-semibold">IPC Classifications:</h5>
+          <ul className="flex flex-wrap gap-2">
+            {hit.document.class_ipcr.map((ipcr) => (
+              <li
+                key={ipcr.symbol + ipcr.action_date}
+                className="text-sm bg-accent text-accent-foreground border-accent-foreground border px-2 py-0.5 rounded-md"
+              >
+                {ipcr.symbol}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function ArticleSection({ hit }: { hit: PatentHit }) {
+  return (
+    <div className="shrink-0 w-[250px] flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <h5 className="font-semibold">Document Preview</h5>
+        <div className="aspect-[1/1.414] border-border border bg-white w-full rounded flex items-start justify-center p-3 text-sm">
+          Preview unavailable
         </div>
       </div>
-      <button
-        onClick={() => setIsExpanded(!isExpanded)}
-        className="w-full p-2 flex justify-center items-center hover:bg-accent"
-      >
-        <ChevronDown className={cn('transition-transform', { 'rotate-180': isExpanded })} />
-      </button>
+      <div className="flex flex-col">
+        <h5 className="font-semibold">History</h5>
+        <div className="flex flex-col divide-y">
+          <div className="py-2">
+            <div className="text-sm">Publication: {hit.document.date_published}</div>
+            <div className="text-sm">
+              {hit.document.jurisdiction} {hit.document.doc_number}
+            </div>
+          </div>
+          <div className="py-2">
+            <div className="text-sm">Application: {hit.document.application_reference.date.toString()}</div>
+            <div className="text-sm">
+              {hit.document.jurisdiction} {hit.document.doc_number}
+            </div>
+          </div>
+          {hit.document.priority_claim.map((priority) => (
+            <div className="py-2">
+              <div className="text-sm">Priority: {priority.date.toString()}</div>
+              <div className="text-sm">
+                {priority.jurisdiction} {priority.doc_number}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
