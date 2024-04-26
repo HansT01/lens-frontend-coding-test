448c5b7d952400b7d27420e05ff00e7a578ec7ff
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-04-26 19:37:06 +1000

    removed redundancy

--- src/components/search/PatentList.tsx
+++ src/components/search/PatentList.tsx
@@ -43,9 +43,6 @@ export function PatentListItem({ hit, zebra }: { hit: PatentHit; zebra?: boolean
           <div className="text-sm">
             Family: {hit.document.family.simple.size}s / {hit.document.family.extended.size}ex
           </div>
-          <div className="text-sm">
-            Family: {hit.document.family.simple.size}s / {hit.document.family.extended.size}ex
-          </div>
           <div className="text-sm">Legal Status: {hit.document.legal_status.patent_status}</div>
           <div className="text-sm">Application Number: {hit.document.application_reference.doc_number}</div>
           <div className="text-sm">Filed: {hit.document.application_reference.date.toString()}</div>
