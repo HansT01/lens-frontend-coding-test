834b85e0447aa3ec4bc0a912eb03652b7ef6eff9
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-04-26 17:11:54 +1000

    cleaned up classes

--- src/components/search/PatentList.tsx
+++ src/components/search/PatentList.tsx
@@ -1,8 +1,12 @@
+import { cn } from '@/lib/utils'
+import { ChevronDown } from 'lucide-react'
 import { Link } from 'raviger'
+import { useState } from 'react'
 import { PatentHit } from '../../models/PatentHit'
 import { PatentSearchResponse } from '../../models/PatentSearchResponse'
 import { PatentPills } from '../PatentPills'
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const getTitle = (hit: PatentHit) => hit.document.title?.en?.at(0)?.text || ''
 
 export function PatentList({ response }: { response: PatentSearchResponse }) {
@@ -17,27 +21,26 @@ export function PatentList({ response }: { response: PatentSearchResponse }) {
 }
 
 export function PatentListItem({ hit, zebra }: { hit: PatentHit; zebra?: boolean }) {
+  const [isOpen, setIsOpen] = useState(false)
+  console.log(hit)
   const className = zebra ? '' : 'bg-slate-100 dark:bg-[#26323b]'
   return (
-    <div key={hit.document.record_lens_id} className={`block p-3 cursor-pointer ${className}`}>
+    <div key={hit.document.record_lens_id} className={cn('p-3 flex flex-col gap-1', className)}>
       <Link href={`/patent/${hit.document.record_lens_id}`}>
-        <h4 className={'text-lg'}>{getTitle(hit)}</h4>
+        <h4 className="text-lg">{getTitle(hit)}</h4>
       </Link>
-      <div className={'py-2'}>
+      <div className="py-2">
         <PatentPills doc={hit.document} />
       </div>
-      <div className={'capitalize py-2 text-sm'}>{hit.document.publication_type}</div>
-      <div className={'py-2 text-sm'}>
+      <div className="text-sm">{hit.document.publication_type}</div>
+      <div className="text-sm">
         Published: {hit.document.date_published || hit.document.year_published || 'unknown'}
       </div>
-      <div>
-        <small>Inventors: </small>
-        {hit.document.inventor?.map((inventor) => <small key={inventor.name}>{inventor.name} </small>)}
-      </div>
-      <div>
-        <small>Applicants: </small>
-        {hit.document.applicant?.map((applicant) => <small key={applicant.name}>{applicant.name} </small>)}
-      </div>
+      <div className="text-sm">Inventors: {hit.document.inventor?.map((inventor) => inventor.name).join(', ')}</div>
+      <div className="text-sm">Applicants: {hit.document.applicant?.map((applicant) => applicant.name).join(', ')}</div>
+      <button onClick={() => setIsOpen(!isOpen)} className="w-full p-2 flex justify-center items-center">
+        <ChevronDown className={cn('transition-transform', { 'rotate-180': isOpen })} />
+      </button>
     </div>
   )
 }
