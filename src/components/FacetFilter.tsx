df0e7fb3279ee99230f14dae8bcb28de32310249
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-04-27 13:06:07 +1000

    dedicated component for jurisdiction filter item with flag

--- src/components/FacetFilter.tsx
+++ src/components/FacetFilter.tsx
@@ -1,4 +1,5 @@
 import { jurisdictionName } from '@/jurisdictions'
+import { Bucket } from '@/models/Bucket'
 import { PatentTermsAggregationKey } from '@/models/PatentTermsAggregationKey'
 import { useTranslation } from 'react-i18next'
 import { TermsAggregation } from '../models/TermsAggregation'
@@ -8,6 +9,45 @@ export interface FacetFilterGroupProps {
   aggregationKey: PatentTermsAggregationKey
   aggregation: TermsAggregation
   label: string
+}
+
+export function JurisdictionFilterItem({ jurisdiction }: { jurisdiction: string }) {
+  const flagUrl = `https://static.lens.org/lens/9.1.3/img/flags/${jurisdiction}.png`
+  return (
+    <div className="flex gap-1 items-center">
+      <span className="h-[13px] shrink-0">
+        <img className="max-h-full" src={flagUrl} alt={jurisdiction} />
+      </span>
+      <div className="text-ellipsis line-clamp-1">{jurisdictionName[jurisdiction] ?? jurisdiction}</div>
+    </div>
+  )
+}
+
+export function FacetFilterItem({
+  aggregationKey,
+  bucket
+}: {
+  aggregationKey: PatentTermsAggregationKey
+  bucket: Bucket
+}) {
+  return (
+    <label
+      className="flex text-xs small cursor-pointer py-1 hover:bg-slate-100 dark:hover:bg-slate-900"
+      key={bucket.key}
+    >
+      <span className="mr-2">
+        <input type="checkbox" />
+      </span>
+      <span className="flex-1">
+        {aggregationKey === PatentTermsAggregationKey.Jurisdiction ? (
+          <JurisdictionFilterItem jurisdiction={bucket.key} />
+        ) : (
+          bucket.key.replace('_', ' ')
+        )}
+      </span>
+      <span className="justify-self-end">({bucket.doc_count.toLocaleString()})</span>
+    </label>
+  )
 }
 
 export function FacetFilter({
@@ -27,30 +67,5 @@ export function FacetFilter({
         <Button size="sm">{t('Refine')}</Button>
       </div>
     </div>
-  )
-}
-
-export function FacetFilterItem({
-  aggregationKey,
-  bucket
-}: {
-  aggregationKey: PatentTermsAggregationKey
-  bucket: any
-}) {
-  return (
-    <label
-      className="flex text-sm small cursor-pointer py-1 hover:bg-slate-100 dark:hover:bg-slate-900"
-      key={bucket.key}
-    >
-      <span className="mr-1">
-        <input type="checkbox" />
-      </span>
-      <span className="flex-1 text-ellipsis line-clamp-1">
-        {(aggregationKey === PatentTermsAggregationKey.Jurisdiction
-          ? jurisdictionName[bucket.key]
-          : bucket.key.replaceAll('_', ' ')) ?? bucket.key}
-      </span>
-      <span className="justify-self-end">({bucket.doc_count.toLocaleString()})</span>
-    </label>
   )
 }
